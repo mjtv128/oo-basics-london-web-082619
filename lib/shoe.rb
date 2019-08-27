dbad7afca2303ed6d58074432500e30ca3f44728
jocular-source-7570 <mjtvan@gmail.com>
2019-08-27 21:54:54 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -4,9 +4,10 @@ class Shoe
   def initialize(brand)
     @brand = brand 
   end 
-  
-  def cobble 
-    puts "The shoe has been repaired"
-  end 
+ 
   
 end 
+
+ def cobble 
+    puts "The shoe has been repaired"
+  end
